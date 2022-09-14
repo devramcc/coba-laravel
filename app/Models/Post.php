f530c919c2fa8aa1511339210c383d871735ebb5
Ramadhan <dev.ramddd@gmail.com>
2022-09-14 21:56:37 +0700

    Belajar Laravel 8 | 7. Post Model | Part 1:End Membuat Model Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Halaman 1",
-            "author" => "Ramadhan",
-            "slug" => "halaman-1",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium delectus exercitationem quo officiis, ullam praesentium eligendi culpa recusandae incidunt, eius officia. Odit dolores illum dolore veniam eum asperiores, voluptatem aut soluta numquam dignissimos est aspernatur id, iusto laboriosam voluptatibus. Sint corporis eligendi optio suscipit qui quo necessitatibus mollitia cum. Repudiandae, iusto quia necessitatibus ea illum hic, ex eveniet corrupti laboriosam laborum aperiam molestias quos harum earum quam sequi explicabo ipsum voluptas a reiciendis consequatur maiores voluptatem qui. Dignissimos, excepturi qui?"
-        ],
-        [
-            "title" => "Halaman 2",
-            "author" => "Ramadhan",
-            "slug" => "halaman-2",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium delectus exercitationem quo officiis, ullam praesentium eligendi culpa recusandae incidunt, eius officia. Odit dolores illum dolore veniam eum asperiores, voluptatem aut soluta numquam dignissimos est aspernatur id, iusto laboriosam voluptatibus. Sint corporis eligendi optio suscipit qui quo necessitatibus mollitia cum. Repudiandae, iusto quia necessitatibus ea illum hic, ex eveniet corrupti laboriosam laborum aperiam molestias quos harum earum quam sequi explicabo ipsum voluptas a reiciendis consequatur maiores voluptatem qui. Dignissimos, excepturi qui?"
-        ]
-    ];
-
-    public static function all() {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug) {
-
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-        
-    }
+    use HasFactory;
 }
