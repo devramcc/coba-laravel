c9f427488c59525431d08e3da28125394ed6e234
Ramadhan <dev.ramddd@gmail.com>
2022-09-17 17:46:42 +0700

    Belajar Laravel 8 | 7. Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,10 @@ class PostController extends Controller
         ]);
     }
 
-    public function show($slug) {
+    public function show(Post $post) {
         return view('post', [
-            "title" => "halaman 1",
-            "post" => Post::find($slug)
+            "title" => $post,
+            "post" => $post
         ]);
     }
 }
